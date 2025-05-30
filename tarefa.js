d3a2e686e37bd4864744097582b1924aae3db255
Julia Degrava <juhcdgomes@gmail.com>
2025-05-30 09:45:29 -0300

    Feat: Remove a última tarefa

--- tarefa.js
+++ tarefa.js
@@ -1,4 +1,9 @@
+// Etapa 1
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
 
 tarefas.push('Fazer a unha', 'Lavar o cabelo');
 console.log(`Lista de tarefas atualizada: ${tarefas}`);
+
+// Etapa 2
+let removerUltimaTarefa = tarefas.pop();
+console.log(`Última tarefa foi removida: ${removerUltimaTarefa}`);
