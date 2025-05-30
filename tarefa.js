c014cb4f640acc75f8a65d6ee51c9a85df4867d5
Julia Degrava <juhcdgomes@gmail.com>
2025-05-30 09:36:48 -0300

    Feat: Adiciona novas tarefas

---
+++ tarefa.js
@@ -0,0 +1,4 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+tarefas.push('Fazer a unha', 'Lavar o cabelo');
+console.log(`Lista de tarefas atualizada: ${tarefas}`);
